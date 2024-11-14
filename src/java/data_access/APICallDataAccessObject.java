bacbc672d37406e2ec8d57a27a21d9a84d6dd312
Sill <158129203+Silly-String@users.noreply.github.com>
2024-11-14 04:56:40 -0500

    just made the data access classes implement the interface. no code inserted yet

--- src/java/data_access/APICallDataAccessObject.java
+++ src/java/data_access/APICallDataAccessObject.java
@@ -1,4 +1,12 @@
 package java.data_access;
 
-public class APICallDataAccessObject {
+import java.entities.Flight;
+import java.use_case.SearchByAirlineID.SearchByAirlineIDDataAccessInterface;
+import java.util.List;
+
+public class APICallDataAccessObject implements SearchByAirlineIDDataAccessInterface {
+    @Override
+    public List<Flight> getFlightsByAirlineId(String airlineId) {
+        return List.of();
+    }
 }
--- src/java/data_access/InFileDataAcessObject.java
+++ src/java/data_access/InFileDataAcessObject.java
@@ -1,4 +1,12 @@
 package java.data_access;
 
-public class InFileDataAcessObject {
+import java.entities.Flight;
+import java.use_case.SearchByAirlineID.SearchByAirlineIDDataAccessInterface;
+import java.util.List;
+
+public class InFileDataAcessObject implements SearchByAirlineIDDataAccessInterface {
+    @Override
+    public List<Flight> getFlightsByAirlineId(String airlineId) {
+        return List.of();
+    }
 }
