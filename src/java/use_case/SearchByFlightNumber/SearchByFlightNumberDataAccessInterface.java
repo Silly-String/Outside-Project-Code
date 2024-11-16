5230ddce27d2cf5bf07c9fce898dc2eace0e5713
Sill <158129203+Silly-String@users.noreply.github.com>
2024-11-16 04:23:39 -0500

    Refactored the term "User" out of SearchByFlightNumberUserDataAccessInterface.java

--- src/java/use_case/SearchByFlightNumber/SearchByFlightNumberDataAccessInterface.java
+++ src/java/use_case/SearchByFlightNumber/SearchByFlightNumberDataAccessInterface.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * The interface of the DAO for the SearchByFlightNumber Use Case.
  */
-public interface SearchByFlightNumberUserDataAccessInterface {
+public interface SearchByFlightNumberDataAccessInterface {
 
     /**
      * Retrieves a list of flights from the system.
